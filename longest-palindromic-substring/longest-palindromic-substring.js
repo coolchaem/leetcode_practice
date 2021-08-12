2d9fda00a1fc4913dbaa781239f332c3b9b06f72
coolchaem <coals6751@gmail.com>
2021-08-12 11:00:48 +0900

    Time: 184 ms (48.41%), Space: 45.8 MB (28.38%) - LeetHub

---
+++ longest-palindromic-substring/longest-palindromic-substring.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let ll = 0, rr = 0;
+    
+    for (let i = 0; i < s.length; i += 1) {
+        for (let j of [i, i+1]) {
+            for (let l = i, r = j; s[l] && s[l] === s[r]; l--, r++) {
+                [ll, rr] = (r-l+1) > (rr-ll+1) ? [l, r] : [ll, rr];
+            }
+        }
+    }
+    
+    return s.substring(ll, rr+1);    
+};
