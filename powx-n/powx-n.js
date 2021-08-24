2a22256e54c7a7258e30420e303bb836b6824234
coolchaem <coals6751@gmail.com>
2021-08-24 22:45:22 +0900

    Time: 88 ms (23.06%), Space: 39 MB (99.80%) - LeetHub

---
+++ powx-n/powx-n.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+  if (n === 0) {
+    return 1;
+  }
+  let pow = Math.abs(n);
+  let result = pow % 2 === 0 ? myPow(x * x, pow / 2) : x * myPow(x * x, (pow - 1) / 2);
+  return Math.sign(n) < 0 ? (1 / result) : result;
+};
