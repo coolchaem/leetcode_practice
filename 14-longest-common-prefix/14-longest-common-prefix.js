5e7e078b20796586956f3ebfea7b543196cb91f5
coolchaem <coals6751@gmail.com>
2022-04-16 09:13:47 +0900

    Time: 77 ms (66.62%), Space: 42.3 MB (83.68%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -7,10 +7,10 @@ var longestCommonPrefix = function(strs) {
   while(cIndex < strs[0].length) {    
     for (let i = 1; i < strs.length; i += 1) {
       if (strs[i].length === cIndex || strs[i][cIndex] !== strs[0][cIndex]) {
-        return strs[0].slice(0, cIndex);
+        return strs[0].substring(0, cIndex);
       }
     }
     cIndex += 1;
   }
-  return strs[0].slice(0, cIndex);
+  return strs[0].substring(0, cIndex);
 };
