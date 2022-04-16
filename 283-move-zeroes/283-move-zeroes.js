56006d58fd1378426b79d8d39e10a3eb6892ea6e
coolchaem <coals6751@gmail.com>
2022-04-16 11:06:57 +0900

    Time: 124 ms (52.49%), Space: 46.7 MB (63.31%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let i = 0;
+  while (i < nums.length) {
+    let j = i + 1, nextZero = i;
+    while (nums[i] === 0 && j < nums.length) {
+      if (nums[j] !== 0) {
+        nums[i] = nums[j];
+        nums[j] = 0;
+        break;
+      }   
+      j += 1;
+    }
+    i += 1;    
+  }
+};
